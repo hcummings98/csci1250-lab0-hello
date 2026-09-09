7c802f161a257b34428271c0b9ad76c315004bf3
Howard Cummings <hlc125@gmail.com>
2026-09-09 11:06:03 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Howard Cummings
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Howard Cummings.");
+Console.WriteLine("This is my first program at ETSU.");
